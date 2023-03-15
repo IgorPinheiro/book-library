2112a9858604180144f51c62ea9db108c6a41b1d
Igor Pinheiro <igor.warlog@gmail.com>
2023-03-15 17:53:51 -0300

    Create DB, Workspace Admin, views.

--- book/models.py
+++ book/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Books(models.Model):
+    name = models.CharField(max_length=100)
+    autor = models.CharField(max_length = 30)
+    co_autor = models.CharField(max_length = 30, blank=True, null= True)
+    data_cadastro = models.DateField(default=date.today)
+    emprestado = models.BooleanField(default = False)
+    nome_emprestado = models.CharField(max_length = 30, blank=True, null= True)
+    data_emprestimo = models.DateTimeField(blank=True, null= True)
+    data_devolucao = models.DateTimeField(blank=True, null= True)
+    tempo_emprestimo = models.DateField(blank=True, null= True)
+
+    class Meta:
+        verbose_name = 'Book'
+
+    def __str__(self):
+        return self.name
+    
